8accc92f93c6c401025e49757c118fe110a962bc
robinsongz <mrrobinsung@gmail.com>
2019-06-18 21:22:56 -0700

    Updates

---
+++ precache-manifest.11d564c8138a152f4dc10855bd55b7bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cf2223476623badde989d125ca80da",
+    "url": "/watawasushi/index.html"
+  },
+  {
+    "revision": "7007b34b2f1a76956317",
+    "url": "/watawasushi/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "94c45cca894aebc522d5",
+    "url": "/watawasushi/static/css/main.968e30b1.chunk.css"
+  },
+  {
+    "revision": "7007b34b2f1a76956317",
+    "url": "/watawasushi/static/js/2.49907be7.chunk.js"
+  },
+  {
+    "revision": "94c45cca894aebc522d5",
+    "url": "/watawasushi/static/js/main.4055bb52.chunk.js"
+  },
+  {
+    "revision": "ab85266e51959b8dfc0a",
+    "url": "/watawasushi/static/js/runtime~main.fa7a6150.js"
+  },
+  {
+    "revision": "f401366193520cdf512c7ade98260e27",
+    "url": "/watawasushi/static/media/ProximaNovaBlack.f4013661.otf"
+  },
+  {
+    "revision": "8f0bc01ce5e5becef482d277cb72b728",
+    "url": "/watawasushi/static/media/ProximaNovaThin.8f0bc01c.otf"
+  },
+  {
+    "revision": "5d4f61a485ef1268d6ed3145d31f9916",
+    "url": "/watawasushi/static/media/sushi.5d4f61a4.jpg"
+  },
+  {
+    "revision": "d08d3596dcd0f6d9ec1dd8e6b0f38f9f",
+    "url": "/watawasushi/static/media/sushi2.d08d3596.jpg"
+  },
+  {
+    "revision": "8f6b5d3d91374526ff653c81c067ee55",
+    "url": "/watawasushi/static/media/sushi3.8f6b5d3d.jpg"
+  },
+  {
+    "revision": "3400fca72d5c1202aa24b255882634d3",
+    "url": "/watawasushi/static/media/sushi4.3400fca7.jpg"
+  },
+  {
+    "revision": "fd354509049b65c30c599f7a198d3e09",
+    "url": "/watawasushi/static/media/sushi5.fd354509.jpg"
+  }
+]);
